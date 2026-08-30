796f632d7bc7fdf1e0e8261e0a39861d14a7dfeb
Kagarumi <Kurai32761@gmail.com>
2026-08-30 16:59:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
   customEmoji: true,  // true = use custom emoji IDs from emoji.js, false = use default unicode
   emojiTheme: "redwhite", // active custom emoji theme key in emoji.js
   helpBannerUrl: "https://i.ibb.co/GfTxbJfC/7-edited.png", // Optional: set a direct image URL to show an inline banner in /help
-  activityName: "YouTube Music", 
+  activityName: "Skabidi dop dop yes yes", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5,
